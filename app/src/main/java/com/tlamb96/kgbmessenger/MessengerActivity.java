a392557b888674ac7864d5c3bdfdf0fb202ffeb7
Tyler Lambert <lambertyler@gmail.com>
2018-04-13 11:51:02 -0400

    created convo and scroll to bottom on msg send

--- app/src/main/java/com/tlamb96/kgbmessenger/MessengerActivity.java
+++ app/src/main/java/com/tlamb96/kgbmessenger/MessengerActivity.java
@@ -35,18 +35,34 @@ public class MessengerActivity extends AppCompatActivity {
             add(new Message(R.string.user, "no", "2:22 am", false));
             add(new Message(R.string.nikolai, "Omg Katya you're being so transparent...", "7:16 am", true));
             add(new Message(R.string.crenshaw, "LOL you should deport her", "7:28 am", true));
-            add(new Message(R.string.user, "Why am I in this gc?", "7:48 am", false));
-            add(new Message(R.string.katya, "DEPORT me!? Where tf would you send me!?? I'm already in Russia", "8:02 am", true));
+            add(new Message(R.string.user, "Why am I in this gc again?", "7:48 am", false));
+            add(new Message(R.string.katya, "DEPORT me!? Where tf would you send me!?? I'm already stuck living in Russia", "8:02 am", true));
             add(new Message(R.string.boris, "Pls don't deport me", "8:05 am", true));
-            add(new Message(R.string.katya, "Boris no one is talking about you", "8:05 am", true));
-            add(new Message(R.string.user, "Mind your own goddamn business", "1:10 pm", false));
+            add(new Message(R.string.katya, "Boris no one is talking about you", "8:06 am", true));
+            add(new Message(R.string.nikolai, "Omg he's such a moron", "8:10 am", true));
+            add(new Message(R.string.crenshaw, "ikr", "8:11 am", true));
+            add(new Message(R.string.nikolai, "Remember that time he gave away the password to all KGB systems?", "8:12 am", true));
+            add(new Message(R.string.crenshaw, "Yeah, all they had to do was ask for it", "8:13 am", true));
+            add(new Message(R.string.katya, "You're joking, right? No one is that dumb", "8:13 am", true));
+            add(new Message(R.string.crenshaw, "I'm 100% serious", "8:13 am", true));
+            add(new Message(R.string.boris, "Well that's not all they had to do", "8:15 am", true));
+            add(new Message(R.string.katya, "Wait, why do all KGB systems have the same password?", "9:20 am", true));
+            add(new Message(R.string.nikolai, "We got tired of writing them down on sticky notes so we held a meeting and agreed on a password for the entire department", "9:22 am", true));
+            add(new Message(R.string.crenshaw, "Idk why we didn't think of this solution earlier", "9:25 am", true));
+            add(new Message(R.string.katya, "Does Boris know the password?", "9:26 am", true));
+            add(new Message(R.string.nikolai, "Nah, he only has the password for his personal computer which is different than the dept's password", "9:27 am", true));
+            add(new Message(R.string.crenshaw, "You thought we'd tell him again? If he told someone we would have to hold another dept meeting to come up with a new one", "9:28 am", true));
+            add(new Message(R.string.nikolai, "It took us three hours to agree on one last time", "9:27 am", true));
         }};
 
         // Setup the recycler view that displays the individual text messages.
         mMessagesRecyclerView = (RecyclerView) findViewById(R.id.reyclerview_message_list);
         mMessageListAdapter = new MessageListAdapter(this, mMessages);
         mMessagesRecyclerView.setAdapter(mMessageListAdapter);
-        mMessagesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        mMessagesRecyclerView.setLayoutManager(linearLayoutManager);
         mMessagesRecyclerView.setNestedScrollingEnabled(false);
     }
 
@@ -69,6 +85,9 @@ public class MessengerActivity extends AppCompatActivity {
         }
         mMessages.add(new Message(R.string.user, textMessage, getCurrentHoursMinutes(), false));
         mMessageListAdapter.notifyDataSetChanged();
+
+        // Scroll to bottom and clear textbox.
+        mMessagesRecyclerView.smoothScrollToPosition(mMessagesRecyclerView.getAdapter().getItemCount()-1);
         messageTextbox.setText("");
     }
 }
